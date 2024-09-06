403f339e96816beee8354b4c83e043a85dba187d
TAn <tanvd357@gmail.com>
2024-09-06 15:57:16 +0700

    OpenCV - Getting Started with Videos

--- opencv/display_image.py
+++ opencv/display_image.py
@@ -6,10 +6,13 @@ import sys
 print( cv.__version__ )
 
 img = cv.imread(cv.samples.findFile("data/starry_night.jpg"))
- 
+
 if img is None:
     sys.exit("Could not read the image.")
-    
+
+font = cv.FONT_HERSHEY_SIMPLEX
+cv.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv.LINE_AA)
+
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
 
